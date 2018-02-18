4e485104152f316be775cbfe2d28b04692b0ae5e
Ghostenshell <34862936+Ghostenshell@users.noreply.github.com>
2018-02-17 19:47:05 -0500

    Create tst.java

---
+++ tst.java
@@ -0,0 +1,39 @@
+public class Manager
+
+{
+
+      public int i;
+
+      private String strDepartment;
+
+       public Manager()
+
+      {   
+
+      }
+
+       public Manager (String Department)
+
+      {
+
+      }
+
+      public void setDepartment(String Department)
+
+      {
+
+      }
+
+      public String getDepartment()
+
+      {
+
+      }
+
+      public String toString()
+
+      {
+
+      }
+
+}    
